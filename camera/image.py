4f958ff8b671c141f07d4f3099f0cad7964ea248
AndreasDTU <s235455@dtu.dk>
2026-03-04 09:03:30 +0100

    plan

--- camera/image.py
+++ camera/image.py
@@ -1,6 +1,8 @@
 
 import cv2
 
+x_max = 1920
+y_max = 1080
 
 
 
@@ -12,4 +14,18 @@ while True:
     cv2.imshow("hsv", hsv)
     cv2.waitKey(1)    # wait for 1 millisecond before moving on to next frame
     cv2.imwrite("filename.jpg", hsv)
+
+#Make 2d array for coordinate system
+
+
+#Some shit like this idk???
+coordinate_array = [[0 for _ in range(x_max)] for _ in range(y_max)]  # Initialize a 2D array for 640x480 image coordinates
+
+#Make picture fit altså fix lens distortion
+
+#Pitcture from hls to 5 colors(Wall, Ball, Orange Ball, Robot, Floor)
+#Color gets an id
+#Fill 2d array with color values
+
+
 

b0048ed3cba7d1be62f584fd1f7728d3f15bd4f8
AndreasDTU <s235455@dtu.dk>
2026-03-04 09:09:20 +0100

    With cam

--- camera/image.py
+++ camera/image.py
@@ -10,7 +10,7 @@ cam = cv2.VideoCapture(1)  # Logitech USB)
 image = cv2.imread("Bane.jpg")
 while True:  
     s, img = cam.read()
-    hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     cv2.imshow("hsv", hsv)
     cv2.waitKey(1)    # wait for 1 millisecond before moving on to next frame
     cv2.imwrite("filename.jpg", hsv)

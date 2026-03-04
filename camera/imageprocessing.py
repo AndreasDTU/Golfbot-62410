53caffe80e80880701c4e089bd407eacfc3426b4
AndreasDTU <s235455@dtu.dk>
2026-03-04 11:27:26 +0100

    ting

--- camera/imageprocessing.py
+++ camera/imageprocessing.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 #Turn into colorspace of choice, idk if this should go before lens distortion or after idk https://docs.opencv.org/4.x/df/d9d/tutorial_py_colorspaces.html
+#https://learnopencv.com/color-spaces-in-opencv-cpp-python/
 def imageprocessing(img, colorspace):
     return cv2.cvtColor(img,colorspace)
 

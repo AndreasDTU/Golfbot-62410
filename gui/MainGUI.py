e846b0761e95a02daa090f08ca2ed14ed7228289
AndreasDTU <s235455@dtu.dk>
2026-06-23 09:48:15 +0200

    Redo after unload

--- gui/MainGUI.py
+++ gui/MainGUI.py
@@ -985,7 +985,12 @@ class MainGui:
         ):
             self._replan_after_displacement()
 
-        if (self._brain_state == BrainState.DONE): #If brain is done, verify pickups to check for any missed balls before stopping
+        if (self._brain_state == BrainState.DONE
+            and self._last_result.smoothed_ball_coordinates
+            and self._last_result.occupancy_grid is not None
+            and self.robot_pose is not None
+        ): #If brain is done, verify pickups to check for any missed balls before stopping
+            self._needs_verification_snapshot = True
             self._verify_pickups()
             #Could add a return 0 here to end program or someshit. Maybe move dance to here?
 
